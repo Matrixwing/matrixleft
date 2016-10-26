efab7c6d46b552409b41da81768f6abd04b96d48
wellroam <449184990@qq.com>
2016-10-26 17:05:00 +0800

    开发：加密测试

--- api/controllers/ServantController.js
+++ api/controllers/ServantController.js
@@ -5,81 +5,10 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var CryptoJS = require("crypto-js");
+
+
 module.exports = {
-  /*
-  *测试用的假数据
-   */
-  queryTest: function (req, res) {
-    var options = {
-      certificateID: req.param('certificateID', ''),
-    }
-    sails.log.info("输入的certificateID:"+options.certificateID);
-    //生成的假数据
-    var testJson = {
-      "msgNo": "0000",
-      "msgInfo": "查询到了信息",
-      "data": [
-        {
-          "userName": "测试3",
-          "IDCard": "519231231231543532",
-          "gender": "女",
-          "birthday": "2016-10-26",
-          "avatarUrl": "http://image.vsuch.com/vsuch/200911/10/96/8379669535949834792.jpg",
-          "certInfo": [
-            {
-              "certificateID": "000000000002",
-              "workTypeName": "育儿嫂",
-              "rank": "高级",
-              "assessment": "合格",
-              "authorityDate": null,
-              "authorityUnit": null,
-              "serialNumber": null,
-              "score1": "78",
-              "score2": "43",
-              "score3": null,
-              "score4": null
-            },
-            {
-              "certificateID": "000000000003",
-              "workTypeName": "育儿嫂",
-              "rank": "高级",
-              "assessment": "合格",
-              "authorityDate": null,
-              "authorityUnit": null,
-              "serialNumber": null,
-              "score1": "67",
-              "score2": "45",
-              "score3": null,
-              "score4": null
-            }
-          ]
-        },
-        {
-          "userName": "测试2",
-          "IDCard": "519231231231543531",
-          "gender": "女",
-          "birthday": "2016-10-26",
-          "avatarUrl": "http://image.vsuch.com/vsuch/200911/10/96/8379669535949834792.jpg",
-          "certInfo": [
-            {
-              "certificateID": "000000000002",
-              "workTypeName": "育儿嫂",
-              "rank": "高级",
-              "assessment": "合格",
-              "authorityDate": null,
-              "authorityUnit": null,
-              "serialNumber": null,
-              "score1": "78",
-              "score2": "43",
-              "score3": null,
-              "score4": null
-            }
-          ]
-        }
-      ]
-    };
-    return res.send(testJson);
-  },
 
   /**
    * 查询服务员详细信息
@@ -87,7 +16,18 @@ module.exports = {
    * @param   {res}  response
    */
   getCertInfo: function (req, res) {
-    var queryInfo =  req.param('queryInfo','');
+    //var queryInfo =  req.param('queryInfo','');
+
+    //*加密部分  以后需封装
+    var queryInfoD =  req.param('queryInfo','');
+    console.log(queryInfoD);
+    console.log(typeof (queryInfoD));
+    var key = CryptoJS.enc.Utf8.parse('ABCDEF1234567890')
+    var bytes  = CryptoJS.AES.decrypt(queryInfoD,key);
+    console.log(bytes);
+    var queryInfo = bytes.toString(CryptoJS.enc.Utf8);
+    //加密部分结束
+    console.log('加密部分结束',queryInfo);
     var options = new Object();
     sails.log.debug(queryInfo);
     var Regx = /^[A-Za-z0-9]*$/;
