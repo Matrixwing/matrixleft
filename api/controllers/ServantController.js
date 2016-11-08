bea9b8d45246933072302c5e5781015b4597ea65
wellroam <449184990@qq.com>
2016-11-08 15:16:57 +0800

    取消加密

--- api/controllers/ServantController.js
+++ api/controllers/ServantController.js
@@ -19,24 +19,22 @@ module.exports = {
     //var queryInfo =  req.param('queryInfo','');
     console.log(req.cookie);
     console.log(req.session);
-    //*加密部分  以后需封装
-    var queryInfoD =  req.param('queryInfo','');
-    var key = CryptoJS.enc.Hex.parse('1234567812345678');
-    var iv   = CryptoJS.enc.Latin1.parse('Pkcs7');
-    //var bytes  = CryptoJS.AES.decrypt(queryInfoD,key);
-    var bytes = CryptoJS.AES.decrypt(queryInfoD,key,
-      {
-        iv:iv,
-        mode:CryptoJS.mode.CBC,
-        padding:CryptoJS.pad.Pkcs7
-      });
-    var queryInfo = bytes.toString(CryptoJS.enc.Utf8);
-    //加密部分结束
+    ////*加密部分  以后需封装
+    //var queryInfoD =  req.param('queryInfo','');
+    //var key = CryptoJS.enc.Hex.parse('1234567812345678');
+    //var iv   = CryptoJS.enc.Latin1.parse('Pkcs7');
+    ////var bytes  = CryptoJS.AES.decrypt(queryInfoD,key);
+    //var bytes = CryptoJS.AES.decrypt(queryInfoD,key,
+    //  {
+    //    iv:iv,
+    //    mode:CryptoJS.mode.CBC,
+    //    padding:CryptoJS.pad.Pkcs7
+    //  });
+    //var queryInfo = bytes.toString(CryptoJS.enc.Utf8);
+    ////加密部分结束
 
-
-    console.log('加密部分结束',queryInfo);
     var options = new Object();
-    sails.log.debug(queryInfo);
+
     var Regx = /^[A-Za-z0-9]*$/;
     if (Regx.test(queryInfo)) {
       if(queryInfo.length==18 || queryInfo.length==15){
