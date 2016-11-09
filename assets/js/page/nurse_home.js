59529d9e683d25e24f0dfc00c482d7f4b80381a5
ljh <424373544@qq.com>
2016-11-09 14:28:48 +0800

    服务员资料完善/用户名片页面

--- assets/js/page/nurse_home.js
+++ assets/js/page/nurse_home.js
@@ -77,3 +77,8 @@
 	ui.newPlaceholder();
 	
 })(mui, window, document, undefined);
+
+$('#user_card').on('tap',function(){
+			_czc.push(["_trackEvent", "个人主页", "名片", "", "", ""]);
+			window.location.href='user_card.html?userID='+userID;
+		})
