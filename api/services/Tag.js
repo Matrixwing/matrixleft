f69fe5d93c9af3ab2feb2216eda616ad88a4349c
wellroam <449184990@qq.com>
2016-11-16 01:59:28 +0800

    筛选

--- api/services/Tag.js
+++ api/services/Tag.js
@@ -23,7 +23,9 @@ module.exports = {
   },
 
   getServantByTag : function (opts,cb){
-    var queryString = 'SELECT a.userID,ur.userName,ur.`avatarUrl`,SUM(weight) sumweight  FROM (SELECT t.userID,t.tagID FROM `taguserre` t LEFT JOIN `tag` ta ON t.tagID=ta.tagID  WHERE t.tagID=';
+    var queryString = "SELECT a.userID,ur.userName,IFNULL(ur.`avatarUrl`,'') AS avatarUrl,IFNULL(ur.salary,'') AS salary,SUM(weight) sumweight  , SUM(ta.`price`) AS price,"+
+    "(SELECT GROUP_CONCAT(tagName) FROM tag t LEFT JOIN taglevel tl ON t.tagID=tl.tagID  LEFT JOIN taguserre tur ON tur.tagID=t.tagID WHERE t.type=2 AND tur.`userID`=a.userID ) AS sysTag FROM "+
+    " (SELECT ta.tagName,t.userID,t.tagID FROM `taguserre` t LEFT JOIN `tag` ta ON t.tagID=ta.tagID  WHERE t.tagID=";
     console.log(opts.length);
     for(tag in opts){
       console.log(tag);
@@ -33,7 +35,7 @@ module.exports = {
           queryString+=' or t.tagID=';
       }
     }
-    queryString+=' OR ta.type=2) AS a LEFT JOIN `tag` ta ON a.tagID=ta.`tagID` LEFT JOIN USER ur  ON ur.`userID`=a.userID GROUP BY a.userID ORDER BY sumweight  DESC;';
+    queryString+=' OR ta.type=2) AS a LEFT JOIN `tag` ta ON a.tagID=ta.`tagID` LEFT JOIN user ur  ON ur.`userID`=a.userID GROUP BY a.userID ORDER BY sumweight  DESC;';
     console.log(queryString);
     TagList.query(queryString,function(err,result){
       if(err) return cb(err);
