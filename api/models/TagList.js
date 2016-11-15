27193f3e97c74b7f5c80ef05225737adabd1ffc7
wellroam <449184990@qq.com>
2016-11-15 20:42:25 +0800

    筛选

--- api/models/TagList.js
+++ api/models/TagList.js
@@ -14,7 +14,8 @@ module.exports = {
 
   },
   getAllTagList : function(cb){
-    TagList.query('SELECT t.tagID,t.tagName,tl.level,tl.sonID FROM  tagLevel tl RIGHT JOIN tag t ON t.tagID=tl.tagID; ',function(err,allList){
+    TagList.query('SELECT t.tagID,t.tagName,ifnull(tl.level,"") as level,ifnull(tl.sonID,"") as sonID FROM  taglevel tl RIGHT JOIN tag t ON t.tagID=tl.tagID; ',function(err,allList){
+      console.log(err);
       if(err) return cb(err);
       return cb(null,allList)
     })
