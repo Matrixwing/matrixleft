6b04cb326ae09078b87ad554110bc6ab1e8e6ba4
ljh <424373544@qq.com>
2016-12-07 13:04:49 +0800

    支付

--- assets/js/page/payment.js
+++ assets/js/page/payment.js
@@ -148,7 +148,8 @@ $('#pay_sure').on('tap',function(){
 					'signType':data.data.signType,
 					'paySign':data.data.paySign,
 				}
-			pay(paydata)
+			//pay(paydata)
+			onBridgeReady(paydata)
 			}else{
 				mui.toast(data.msgInfo);
 			}	
@@ -160,6 +161,8 @@ $('#pay_sure').on('tap',function(){
 	    	}
 	    }
 	});
+})
+
 	function pay(paydata){
 	    wx.config({
 	        debug: false, 
@@ -191,4 +194,19 @@ $('#pay_sure').on('tap',function(){
 	        });
 	    });
 	}
-})
+function onBridgeReady(paydata){
+	console.log(paydata);
+   WeixinJSBridge.invoke(
+       'getBrandWCPayRequest', {
+       	appId: paydata.appId, 
+        timeStamp: paydata.timeStamp, 
+        nonceStr: paydata.nonceStr,
+        package: paydata.package, 
+        signType: paydata.signType,
+        paySign: paydata.paySign
+       },
+       function(res){     
+           if(res.err_msg == "get_brand_wcpay_request：ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+       }
+   ); 
+}
