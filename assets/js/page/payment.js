c56a059b0f314c65929953fd61d9bd954f600ddb
ljh <424373544@qq.com>
2016-12-09 21:28:32 +0800

    价格计算

--- assets/js/page/payment.js
+++ assets/js/page/payment.js
@@ -17,7 +17,7 @@ $('#st_time').on('tap', function(event) {
 $('#m_num').on('tap', function(event) {
 	var m_num = new mui.PopPicker();
  	m_num.setData([
- 		{value:'1',text:'1月-无优惠',yh:'0'},
+ 		{value:'1',text:'1月-无优惠-手续费0.6%',yh:'0'},
  		{value:'3',text:'3月-立减10元',yh:'10'},
  		{value:'6',text:'6月-立减15元',yh:'15'},
  		{value:'12',text:'12月-立减100元',yh:'100'},
@@ -71,42 +71,48 @@ function all_m(){
 	var commission=0;
 	var yh = $('#m_num').attr('yh');
 	var num = $('#m_num').attr('num');
-	if (!servicePrice) {
-		servicePrice=0;
+	var salary_m=salary*num;
+	var all=0;
+	if (num==1) {
 		$('#commission_div').show();
-		$('#servicePrice_div').hide();
-		commission=base.keepTwoDecimal(((salary*num)*0.006));
-		var a =salary*num+commission;
-		if (a<yh) {
-			$('#yh_div').hide();
-			$('#all_m').html(salary*num+commission);
+		if (!servicePrice) {
+			$('#servicePrice_div').hide();
+			commission=base.keepTwoDecimal(salary_m*0.012);
+			all =salary*num+commission;
 		}else{
-			$('#all_m').html(salary*num+commission-yh);
-			if (yh!=0) {
-				$('#yh_div').show();
-				$('#yh_show').html('-￥'+yh)
-			}else{
-				$('#yh_div').hide();
-			};
+			$('#servicePrice_div').show();
+			commission=base.keepTwoDecimal(salary_m*0.006);
+			all =salary*num+servicePrice*num+commission;
+			$('#servicePrice_show').html('￥'+servicePrice*num)
 		};
 		$('#commission_show').html('￥'+commission)
+		
 	}else{
-		$('#commission_div').hide();
-		$('#servicePrice_div').show();
-		var b =salary*num+servicePrice*num+commission;
-		if (b<yh) {
-			$('#yh_div').hide();
-			$('#all_m').html(salary*num+servicePrice*num+commission)
+		if (!servicePrice) {
+			$('#commission_div').show();
+			$('#servicePrice_div').hide();
+			commission=base.keepTwoDecimal((salary_m*0.006));
+			$('#commission_show').html('￥'+commission)
+			all =salary*num+commission;
 		}else{
-			$('#all_m').html(salary*num+servicePrice*num+commission-yh)
-			if (yh!=0) {
-				$('#yh_div').show();
-				$('#yh_show').html('-￥'+yh)
-			}else{
-				$('#yh_div').hide();
-			};
+			$('#servicePrice_div').show();
+			$('#commission_div').hide();
+			all =salary*num+servicePrice*num;
+			$('#servicePrice_show').html('￥'+servicePrice*num)
 		};
-		$('#servicePrice_show').html('￥'+servicePrice*num)
+	};
+
+	if (all<yh) {
+		$('#yh_div').hide();
+		$('#all_m').html(all)
+	}else{
+		$('#all_m').html(all-yh)
+		if (yh!=0) {
+			$('#yh_div').show();
+			$('#yh_show').html('-￥'+yh)
+		}else{
+			$('#yh_div').hide();
+		};
 	};
 	$('#salary_show').html('￥'+salary*num)
 }
@@ -132,18 +138,34 @@ $('#pay_sure').on('tap',function(){
 	if (!servicePriceID) {
 		servicePriceID=0;
 	};
+	var totalFee=($('#all_m').html())*100;
+	var data={
+    	orderId:orderId,
+    	servicePriceID:servicePriceID,
+    	salary:salary*100,
+    	payUrl:payUrl,
+    	firstService:firstService,
+    	month:num,
+    	commission:commission*100,
+    	platform:'wx',
+    	totalFee:totalFee
+    };
+    //sign方法：按照ASCII码从小到大排序（字典序）为URL键值对的格式（即key1=value1&key2=value2…）后用md5加密
+	function sign(param){
+		var querystring = Object.keys(param).filter(function(key){
+		return param[key] !== undefined && param[key] !== ''  <0;
+		}).sort().map(function(key){
+			return key + '=' + param[key];
+		}).join("&")
+		return $.md5(querystring).toUpperCase();
+	} 
+	
+	var sign= sign(data);
+	data['sign'] = sign;
 	$.ajax({
 	    type: 'post',
 	    url: '/pay',
-	    data:{
-	    	'orderId':orderId,
-	    	'servicePriceID':servicePriceID,
-	    	'salary':salary*100,
-	    	'payUrl':payUrl,
-	    	'firstService':firstService,
-	    	'month':num,
-	    	'commission':commission*100
-	    },
+	    data:data,
 	    dataType: 'json',
 	    success: function(data) {
 	    	$('.tips').hide();
