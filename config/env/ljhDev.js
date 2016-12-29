4c47f93a49818af3d31dd8f7932abe675365ad0f
wellroam <449184990@qq.com>
2016-12-29 22:12:24 +0800

    商务合作

--- config/env/ljhDev.js
+++ config/env/ljhDev.js
@@ -18,6 +18,7 @@ module.exports = {
     notify_url : 'http://15898oq619.iok.la/ok',
     loginPage:'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8ec4c9973e23c290&redirect_uri=' +
     'http%3a%2f%2f15898oq619.iok.la%2fweixinLogIn&response_type=code&scope=snsapi_userinfo&state=needlogin#wechat_redirect',//微信授权页面
+    newCompTemp:'hoOqjOTpH6T0MUdc5eFXFe1YJmIAp-QdJ_iPQkXVr54',//申请加入微元汇通知
   },
   qiniu:{
     avatarBucket : 'test',
